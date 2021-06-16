71f15c3a593ed12492e16964d2e7f1b019435ccb
Tue Thomsen <s163852@student.dtu.dk>
2021-06-16 15:00:52 +0200

    Implementation of datamodule

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,6 +3,7 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 from src.models.model import GCN
 from src.data.make_dataset import get_mutag_data, get_dataloader
+from src.data.datamodule import MUTANGDataModule
 
 wandb.login(key='9bb7c1fffbc3ead322b0944221455e8deaaa7111')
 wandb_logger = WandbLogger(project="Geometric", entity='classy_geometric')
@@ -13,11 +14,10 @@ trainloader = get_dataloader(traindataset)
 testdataset = get_mutag_data(train=False, cleaned=False)
 testloader = get_dataloader(testdataset)
 
+dm = MUTANGDataModule(batch_size=64)
+dataset = dm.prepare_data()
 
-data = next(iter(trainloader))
-model = GCN(traindataset.num_node_features, traindataset.num_classes,lr=0.01,p=0.5)
-model.train()
-out = model(data.x, data.edge_index, data.batch)
+model = GCN(dataset.num_node_features, dataset.num_classes,lr=0.01,p=0.5)
 
 trainer = pl.Trainer(
    logger=wandb_logger,    # W&B integration
@@ -27,6 +27,6 @@ trainer = pl.Trainer(
    deterministic=False,     # keep it deterministic
    )
 
-trainer.fit(model,trainloader,testloader)
+trainer.fit(model,dm)
 
 
