230db8f3306eca057a91f6d1262e953fbbabf8d0
Tue Thomsen <s163852@student.dtu.dk>
2021-06-23 15:36:18 +0200

    fixing paths

--- src/settings/paths.py
+++ src/settings/paths.py
@@ -1,6 +1,6 @@
 import os
 
-MODULE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+MODULE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir,os.path.pardir))
 
 MODELS_PATH = os.path.join(MODULE_PATH, 'models')
 MODEL_STORE_PATH = os.path.join(MODULE_PATH, 'model_store')
@@ -10,3 +10,5 @@ CLEANED_DATA_PATH = os.path.join(DATA_PATH, 'cleaned')
 NOT_CLEANED_DATA_PATH = os.path.join(DATA_PATH, 'not_cleaned')
 VISUALIZATION_PATH = os.path.join(MODULE_PATH, 'reports', 'figures')
 CLOUD_PATH = os.path.join(MODULE_PATH,'src','cloud')
+
+
