19aae44a9bb6f4e4f105dcca9af2a19fb2cb0e15
Christian Hinge <christian@hingepedersen.dk>
2021-06-23 16:45:21 +0200

    aml_main entrance

--- src/cloud/aml_main.py
+++ src/cloud/aml_main.py
@@ -2,6 +2,7 @@ from azureml.core.authentication import (
     AzureCliAuthentication,
     InteractiveLoginAuthentication,
 )
+
 from azureml.core import Workspace
 from azureml.core import Environment
 from azureml.core import ScriptRunConfig
@@ -11,89 +12,60 @@ import argparse
 from src import settings
 import os
 from src.settings import paths
-
+import hydra
 from dotenv import load_dotenv, find_dotenv
-
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "--cpu",
-    action="store_true",
-    type=bool,
-    help="",
-)
-
-parser.add_argument(
-    "--gpu",
-    action="store_true",
-    type=bool,
-    help="",
-)
-
-parser.add_argument(
-    "-e",
-    "--experiment",
-    action="store",
-    type=str,
-    help="",
-    default="exp-3"
-)
-
-args = parser.parse_args()
-
-if args.gpu:
-    target = "geo-gpu"
-elif args.cpu:
-    target = "geo-cpu"
-else:
-    raise Exception("Please choose either gpu or cpu")
-
-ws = Workspace.from_config(os.path.join(paths.CLOUD_PATH,"config.json"))
-
-## Uncomment all below if you are not able to access workspace ##
-
-#interactive_auth = InteractiveLoginAuthentication(
-#    tenant_id="your tenant id",
-#    force=True
-#)
-
-#ws = Workspace(
-#    subscription_id="",
-#    resource_group="Geometric-Group",
-#    workspace_name="geometric-ws",
-#    auth=interactive_auth,
-#)
-
-compute_target = ws.compute_targets[target]
-
-#DOCKER
-env = Environment(name="geo-docker-train")
-env.docker.enabled = True
-env.docker.base_image = None
-env.docker.base_dockerfile = "src/cloud/Dockerfile.train"
-env.python.user_managed_dependencies=True
-#env.python.interpreter_path = "/opt/venv/bin/python"
-env.environment_variables = {
-    "WANDB_KEY":os.getenv("WANDB_KEY")
-}
-
-arguments = ["--aml"]
-
-config = ScriptRunConfig(
-    environment=env,  # set the python environment
-    source_directory='.',
-    script='src/main.py',
-    compute_target = compute_target,
-    arguments = [""]
+from omegaconf import DictConfig
+import sys
 
 
-exp = Experiment(ws, args.experiment)
-run = exp.submit(config)
-print(run.get_portal_url())
+def main():
+    dotenv_path = find_dotenv()
+    load_dotenv(dotenv_path)
+    args  = sys.argv[1:]
+    target = None
+    cfg = configparser.ConfigParser()
+    cfg.read("src/config/aml_config.ini")
 
-# Wait until trial is done to stop compute target
-run.wait_for_completion()
-compute_target.stop(wait_for_completion=True, show_output=True)
+    if "--gpu" in args:
+        target = "geo-gpu"
+        args.remove("--gpu")
+    elif "--cpu" in args:
+        target = "geo-cpu"
+        args.remove("--cpu")
+    else:
+        target = cfg["DEFAULT"]["target"]
+
+    if "-e" in args:
+        exp = args[args.index("-e") + 1]
+        args.remove("-e")
+        args.remove(exp)
+    else:
+        exp = cfg["DEFAULT"]["exp"]
+
+    ws = Workspace.from_config(os.path.join(paths.CLOUD_PATH,"config.json"))
+    compute_target = ws.compute_targets[target]
+
+    #DOCKER
+    env = Environment(name="geo-docker-train")
+    env.docker.enabled = True
+    env.docker.base_image = None
+    env.docker.base_dockerfile = "src/cloud/Dockerfile.train"
+    env.python.user_managed_dependencies=True
+    #env.python.interpreter_path = "/opt/venv/bin/python"
+    env.environment_variables = {
+        "WANDB_KEY":os.getenv("WANDB_KEY")
+    }
+
+    config = ScriptRunConfig(
+        environment=env,  # set the python environment
+        source_directory='.',
+        script='src/main.py',
+        compute_target = compute_target,
+        arguments = ["azure=True"]+args)
+
+    exp = Experiment(ws, exp)
+    run = exp.submit(config)
+    print(run.get_portal_url())
+
+if __name__ == "__main__":
+    main()
