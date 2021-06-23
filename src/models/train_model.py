6a01dee3fea50df0a36eea5c6244f0931e300645
Tue Thomsen <s163852@student.dtu.dk>
2021-06-23 19:10:01 +0200

    Revert "Remove modelname in .yml + enable gpu in train_mod"
    This reverts commit 78fb0340213611e69df0e4c5a72f05564e0f0037.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -65,9 +65,8 @@ def train(
         logger=wandb_logger,  # W&B integration
         max_epochs=epochs,  # number of epochs
         deterministic=True,  # keep it deterministic
-        # default_root_dir=os.path.join(CHECKPOINT_PATH, name + "."),
+        # default_root_dir=os.path.join(CHECKPOINT_PATH, name + ".") ** kwargs,
         callbacks=checkpoint_callback,
-        ** kwargs,
     )
 
     trainer.fit(model, dm)
